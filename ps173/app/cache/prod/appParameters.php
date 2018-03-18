11c9a42c037c3357b773e73fab171fa44aa5ffc0
Pham Hien <phamthithuhien3012@gmail.com>
2018-03-19 01:28:49 +0700

    first

---
+++ ps173/app/cache/prod/appParameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'ttu173',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '1V0Ijj0qI8p63SLgEUZjItkcdiy3goFFOt53stzF8cvDYTZQGy8nZ2dB',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-03-13',
+    'locale' => 'en-US',
+    'cookie_key' => 'HOVm6EOJoGTTSGiYkOyCtKIhNtjnFZWUVoHU6Lv60yk0uTmUbUzjgXFz',
+    'cookie_iv' => 'Uk8GOckP',
+    'new_cookie_key' => 'def00000796a79becfd93375932daee07a987337167f4b262907278c7a230d359a6dbae0aa68a0d74dd048de3b09a85d2a69d7d1121199ae50d83a59bfa26f09c1e5ebf1',
+  ),
+);
